4a83975f3aa0c0353f355ca862d003c33cf708da
Peter Vu <dmtruong1980@gmail.com>
2024-06-15 23:05:46 -0500

    feat: Complete exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,34 +1,6 @@
+import Course from './components/Course'
+
 const Title = ({ title }) => <h1>{title}</h1>
-
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Total = ({ parts }) => (
-  <b>
-    total of {parts.reduce(
-      (total, part) => total + part.exercises, 0
-    )} exercises
-  </b>
-)
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>
-);
-
-const Course = ({ course }) => (
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>
-)
 
 const App = () => {
   const courses = [
